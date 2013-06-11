44203d6b7e01cb7b595043270e5aaa31bbf4023b
pyatachok <pyatachok@pn1>
2013-06-11 22:35:51 +0300

    настройка dashboard http://sonata-project.org/bundles/admin/master/doc/reference/getting_started.html

---
+++ src/AdManager/PublisherBundle/Admin/TagAdmin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AdManager\PublisherBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class TagAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+            ->add('enabled', null, array('required' => false))
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('posts')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('slug')
+            ->add('enabled')
+        ;
+    }
+}
