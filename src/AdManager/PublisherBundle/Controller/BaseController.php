f175ddc8e76bdede77c303718dc889fe097f4e84
pyatachok <pyatachok@pn1>
2013-06-23 20:36:05 +0300

    Список полей: контроллер

---
+++ src/AdManager/PublisherBundle/Controller/BaseController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace AdManager\PublisherBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+use Sonata\AdminBundle\Exception\ModelManagerException;
+//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+/**
+ * Для наследования кастомных классов, здесь определены общие методы удаления 
+ * для создания пометки об удалении
+ *
+ * @author pyatachok
+ */
+abstract class BaseController extends CRUDController
+{
+    
+    /**
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException|\Symfony\Component\Security\Core\Exception\AccessDeniedException
+     *
+     * @param mixed $id
+     *
+     * @return Response|RedirectResponse
+     */
+    public function deleteAction($id)
+    {
+        $id     = $this->get('request')->get($this->admin->getIdParameter());
+        $object = $this->admin->getObject($id);
+
+        if (!$object) {
+            throw new NotFoundHttpException(sprintf('unable to find the publisher-object with id : %s', $id));
+        }
+
+        if (false === $this->admin->isGranted('DELETE', $object)) {
+            throw new AccessDeniedException();
+        }
+
+        if ($this->getRestMethod() == 'DELETE') {
+            try {
+		$object->setDeleted('1');
+                $this->admin->update($object);
+                
+
+                if ($this->isXmlHttpRequest()) {
+                    return $this->renderJson(array('result' => 'ok'));
+                }
+
+                $this->addFlash('sonata_flash_success', 'flash_delete_success');
+
+            } catch (ModelManagerException $e) {
+
+                if ($this->isXmlHttpRequest()) {
+                    return $this->renderJson(array('result' => 'error'));
+                }
+
+                $this->addFlash('sonata_flash_error', 'flash_delete_error');
+            }
+
+            return new RedirectResponse($this->admin->generateUrl('list'));
+        }
+
+        return $this->render($this->admin->getTemplate('delete'), array(
+            'object' => $object,
+            'action' => 'delete'
+        ));
+    }
+    
+   
+    
+    /**
+     * execute a batch delete
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     *
+     * @param \Sonata\AdminBundle\Datagrid\ProxyQueryInterface $query
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function batchActionDelete(ProxyQueryInterface $query)
+    {
+        if (false === $this->admin->isGranted('DELETE')) {
+            throw new AccessDeniedException();
+        }
+
+        try {
+            
+
+            foreach ($query->getQuery()->execute() as $pos => $object) {
+                $object->setDeleted('1');
+                $this->admin->update($object);
+                
+            }
+
+            $this->addFlash('sonata_flash_success', 'flash_batch_delete_success');
+        } catch ( ModelManagerException $e ) {
+            $this->addFlash('sonata_flash_error', 'flash_batch_delete_error');
+        }
+
+        return new RedirectResponse($this->admin->generateUrl('list', array('filter' => $this->admin->getFilterParameters())));
+    }
+    
+}
