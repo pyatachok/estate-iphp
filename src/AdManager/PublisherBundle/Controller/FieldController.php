a1a90454ffb7088c40e95d650398146aa6fbcf1f
pyatachok <pyatachok@pn1>
2013-06-25 23:01:37 +0300

    Список полей:исправлен баг обнаруженый при создании или редактировании полей объявлений(невозможность сохранения из-за ошибки валидации вложенной формы)

--- src/AdManager/PublisherBundle/Controller/FieldController.php
+++ src/AdManager/PublisherBundle/Controller/FieldController.php
@@ -35,7 +35,7 @@ class FieldController extends BaseController
 
         $this->admin->setSubject($object);
 
-        /** @var $form \Symfony\Component\Form\Form */
+        
         $form = $this->createForm(new \AdManager\PublisherBundle\Form\FieldType(), $object);
         $form->add('deleted', 'checkbox', array('label' => 'Is Deleted', 'required' => false,));
         $form->setData($object);
@@ -43,9 +43,7 @@ class FieldController extends BaseController
 
         if ($this->getRestMethod() == 'POST') {
             $form->bind($this->get('request'));
-            error_log(
-                    var_dump(array(get_class($form)))
-            );
+
             $isFormValid = $form->isValid();
 
             // persist if the form was valid and if in preview mode the preview was approved
@@ -88,4 +86,74 @@ class FieldController extends BaseController
         ));
     }
 
+    
+    
+    
+    /**
+     * return the Response object associated to the create action
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     * @return Response
+     */
+    public function createAction()
+    {
+        // the key used to lookup the template
+        $templateKey = 'edit';
+
+        if (false === $this->admin->isGranted('CREATE')) {
+            throw new AccessDeniedException();
+        }
+
+        $object = $this->admin->getNewInstance();
+
+        $this->admin->setSubject($object);
+
+        $form = $this->createForm(new \AdManager\PublisherBundle\Form\FieldType(), $object);
+        $form->add('deleted', 'checkbox', array('label' => 'Is Deleted', 'required' => false,));
+
+        if ($this->getRestMethod()== 'POST') {
+            $form->bind($this->get('request'));
+
+            $isFormValid = $form->isValid();
+
+            // persist if the form was valid and if in preview mode the preview was approved
+            if ($isFormValid && (!$this->isInPreviewMode() || $this->isPreviewApproved())) {
+                $this->admin->create($object);
+
+                if ($this->isXmlHttpRequest()) {
+                    return $this->renderJson(array(
+                        'result' => 'ok',
+                        'objectId' => $this->admin->getNormalizedIdentifier($object)
+                    ));
+                }
+
+                $this->addFlash('sonata_flash_success','flash_create_success');
+                // redirect to edit mode
+                return $this->redirectTo($object);
+            }
+
+            // show an error message if the form failed validation
+            if (!$isFormValid) {
+                if (!$this->isXmlHttpRequest()) {
+                    $this->addFlash('sonata_flash_error', 'flash_create_error');
+                }
+            } elseif ($this->isPreviewRequested()) {
+                // pick the preview template if the form was valid and preview was requested
+                $templateKey = 'preview';
+            }
+        }
+
+        $view = $form->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+
+        return $this->render($this->admin->getTemplate($templateKey), array(
+            'action' => 'create',
+            'form'   => $view,
+            'object' => $object,
+        ));
+    }
+
+    
 }
