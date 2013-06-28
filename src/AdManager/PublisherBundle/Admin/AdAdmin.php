3125b62466a8a1793185653fb798b94f9d6d44b4
pyatachok <pyatachok@pn1>
2013-06-28 23:06:38 +0300

    Список  объявлений отображение

---
+++ src/AdManager/PublisherBundle/Admin/AdAdmin.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace AdManager\PublisherBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Translation\TranslatorInterface;
+
+use Knp\Menu\FactoryInterface as MenuFactoryInterface;
+use Knp\Menu\ItemInterface as MenuItemInterface;
+
+//use Symfony\Component\Translation\Translator;
+//use Sonata\AdminBundle\Translator\Extractor\JMSTranslatorBundle\AdminExtractor;
+
+class AdAdmin extends Admin
+{
+    
+    protected $translationDomain = 'AdManagerPublisherBundle';
+    
+    
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title')
+            ->add('creation_date')
+            ->add('deleted', null, array('required' => false))
+            ->add('publisher', 'sonata_type_model', array(
+                'class' => 'AdManagerPublisherBundle:Publisher',
+                'property' => 'name',
+                'multiple' => false,
+//                'by_reference' => false
+            ));
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('creation_date')
+            ->add('deleted')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('creation_date')
+            ->add('deleted')
+//            ->add('enabled')
+        ;
+    }
+    
+    public function buildBreadcrumbs($action, MenuItemInterface $menu = null)
+    {
+
+        $menu = $this->menuFactory->createItem('root');
+        $menu = $menu->addChild(
+                $this->trans($this->getLabelTranslatorStrategy()->getLabel('dashboard', 'breadcrumb', 'link'), array(), $this->translationDomain, 'ru'),
+                array('uri' => $this->routeGenerator->generate('sonata_admin_dashboard'))
+            );
+        
+        return parent::buildBreadcrumbs($action, $menu);
+    }
+    
+    public function trans($id, array $parameters = array(), $domain = 'AdManagerPublisherBundle', $locale = null)
+    {
+        $domain = $domain ?: $this->translationDomain;
+
+        if (!$this->translator) {
+            return $id;
+        }
+        return $this->translator->trans($id, $parameters,  $this->translationDomain, $locale);
+    }
+    
+
+    
+}
--- src/AdManager/PublisherBundle/Controller/AdController.php
+++ src/AdManager/PublisherBundle/Controller/AdController.php
@@ -2,228 +2,204 @@
 
 namespace AdManager\PublisherBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-
-use AdManager\PublisherBundle\Entity\Ad;
-use AdManager\PublisherBundle\Entity\Publisher;
-use AdManager\PublisherBundle\Form\AdType;
-use AdManager\PublisherBundle\Form\AdFieldValueType;
-class AdController extends Controller
+class AdController extends BaseController
 {
-    private $_breadcrumbs = array(
-        'ad' => array('uri' => '','label' => 'Список объявлений'),
-        'add' => array('uri' => '','label' => 'Добавление'),
-        'edit' => array('uri' => '','label' => 'Редактирование'),
-//        's' => 'Список редакторов объявлений',
-    );
-    
-    public function indexAction()
-    {
-	$ads = $this->getDoctrine()
-		->getRepository('AdManagerPublisherBundle:Ad')
-		->findBy(array(), array('deleted' => 'ASC', 'creation_date' => 'DESC'))
-		;
-	if (!$ads) {
-	    throw $this->createNotFoundException('No ads found');
-	}
-
-	return $this->render('AdManagerPublisherBundle:Ad:index.html.twig', array(
-            'ads' => $ads,
-            'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
-            'admin_pool'    => $this->container->get('sonata.admin.pool'),
-            'breadcrumbs' => $this->getBreadcrumb(),
-            ));
-    }
-    
-    public function showAction($id)
-    {
-	$ad = $this->getDoctrine()
-	    ->getRepository('AdManagerPublisherBundle:Ad')
-	    ->find($id);
-	
-	if (!$ad) {
-	    throw $this->createNotFoundException('No ad found for id = '. $id);
-	}
-	
-	
-        return $this->render('AdManagerPublisherBundle:Ad:show.html.twig', 
-            array(
-                'ad' => $ad,
-                'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
-                'admin_pool'    => $this->container->get('sonata.admin.pool'),
-                'breadcrumbs' => $this->getBreadcrumb(),
-            ));
-    }
-    
-    
-    public function addAction(Request $request)
-    {
-	$ad = new Ad();
-	$form = $this->createForm(new AdType(), $ad);
-	
-	if ($request->getMethod() == 'POST') {
-	    $form->bindRequest($request);
-
-	    if ($form->isValid()) {
-		// выполняем прочие действие, например, сохраняем задачу в базе данных
-		
-		$ad = $form->getData();
-		$ad->setDeleted(0);
-
-		$em = $this->getDoctrine()->getEntityManager();
-		$em->persist($ad);
-		$em->flush();
-		
-		return $this->redirect($this->generateUrl('ad_manager_ad_homepage'));
-	    }
-	}
-	
-	
-	return $this->render('AdManagerPublisherBundle:Ad:add.html.twig', array(
-            'form' => $form->createView(),
-            'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
-            'admin_pool'    => $this->container->get('sonata.admin.pool'),
-            'breadcrumbs' => $this->getBreadcrumb(),
-        ));
-    }
-    
-    public function deleteAction($id, $confirm = 0)
-    {
-	$ad = $this->getDoctrine()
-	    ->getRepository('AdManagerPublisherBundle:Ad')
-	    ->find($id);
-	if ($ad->getDeleted() == 1)
-	{
-	    return $this->redirect($this->generateUrl('ad_manager_ad_homepage'));
-	}
-	    
-	$request = $this->getRequest();
-	
-	if ($request->getMethod() == 'POST') {
-	    $form = $request->request->get('form');
-	    
-	    if ($form['confirm'] == 1)
-	    {
-		$em = $this->getDoctrine()->getEntityManager();
-		$ad->setDeleted('1');
-		$em->flush();
-		return $this->redirect($this->generateUrl('ad_manager_ad_homepage'));
-	    }
-	    
-	} else {
-	    $form = $this->createFormBuilder()
-		->add('confirm', 'checkbox', array('label' => 'Do you realy want to delete advertisment'))
-		->getForm();
-	}
-	
-	if (!$ad) {
-	    throw $this->createNotFoundException('No ad found for id = '. $id);
-	}
-        return $this->render(
-		'AdManagerPublisherBundle:Ad:delete.html.twig', 
-		array(
-		    'ad' => $ad,
-		    'form' =>  $form->createView(),
-                    'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
-                    'admin_pool'    => $this->container->get('sonata.admin.pool'),
-                    'breadcrumbs' => $this->getBreadcrumb(),
-		    ));
-    }
-    
-    public function editAction($id)
-    {
-	$ad = $this->getDoctrine()
-	    ->getRepository('AdManagerPublisherBundle:Ad')
-	    ->find($id);
-	
-	if (!$ad) {
-	    throw $this->createNotFoundException('No advertisment found for id = '. $id);
-	}
-
-	{
-	    /**
-	     * Составим массив полей объявления до внесения изменений
-	     */
-
-	    $beforUpdateFields = array();
-	    foreach ($ad->getFieldValues() as $fieldValue)
-	    {
-		$beforUpdateFields[$fieldValue->getId()] = $fieldValue;
-	    }
-	}
-	
-	$editForm = $this->createForm(new AdType(), $ad);
-	$editForm->add('deleted', 'checkbox', array(
-	    'label' => 'Is Deleted', 
-	    'required' => false,
-	    ));	
-	$request = $this->getRequest();
-	$em = $this->getDoctrine()->getEntityManager();
-	
-	if ($request->getMethod() == 'POST') {
-	    $editForm->bindRequest($request);
-
-	    if ($editForm->isValid()) {
-		$ad = $editForm->getData();
-		
-		if ( isset($_REQUEST['ad']['field_values']))
-		{
-		    $notDeletedIds = array_unique($this->getElementsByarrayIndex($_REQUEST['ad']['field_values'], 'id'));
-
-		    foreach($beforUpdateFields as $key => $value)
-		    {
-			if (!in_array($key, $notDeletedIds))
-			{
-			    $em->remove($value);
-			}
-		    }
-		}
+//    
+//    public function indexAction()
+//    {
+//	$ads = $this->getDoctrine()
+//		->getRepository('AdManagerPublisherBundle:Ad')
+//		->findBy(array(), array('deleted' => 'ASC', 'creation_date' => 'DESC'))
+//		;
+//	if (!$ads) {
+//	    throw $this->createNotFoundException('No ads found');
+//	}
+//
+//	return $this->render('AdManagerPublisherBundle:Ad:index.html.twig', array(
+//            'ads' => $ads,
+//            'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
+//            'admin_pool'    => $this->container->get('sonata.admin.pool'),
+//            'breadcrumbs' => $this->getBreadcrumb(),
+//            ));
+//    }
+//    
+//    public function showAction($id)
+//    {
+//	$ad = $this->getDoctrine()
+//	    ->getRepository('AdManagerPublisherBundle:Ad')
+//	    ->find($id);
+//	
+//	if (!$ad) {
+//	    throw $this->createNotFoundException('No ad found for id = '. $id);
+//	}
+//	
+//	
+//        return $this->render('AdManagerPublisherBundle:Ad:show.html.twig', 
+//            array(
+//                'ad' => $ad,
+//                'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
+//                'admin_pool'    => $this->container->get('sonata.admin.pool'),
+//                'breadcrumbs' => $this->getBreadcrumb(),
+//            ));
+//    }
+//    
+//    
+//    public function addAction(Request $request)
+//    {
+//	$ad = new Ad();
+//	$form = $this->createForm(new AdType(), $ad);
+//	
+//	if ($request->getMethod() == 'POST') {
+//	    $form->bindRequest($request);
+//
+//	    if ($form->isValid()) {
+//		// выполняем прочие действие, например, сохраняем задачу в базе данных
+//		
+//		$ad = $form->getData();
 //		$ad->setDeleted(0);
-		$em->persist($ad);
-		$em->flush();
-		
-		return $this->redirect($this->generateUrl('ad_manager_ad_homepage'));
-	    }
-	}
-	
-        return $this->render(
-		'AdManagerPublisherBundle:Ad:edit.html.twig', 
-		array(
-		    'ad' => $ad,
-		    'form' =>  $editForm->createView(),
-                    'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
-                    'admin_pool'    => $this->container->get('sonata.admin.pool'),
-                    'breadcrumbs' => $this->getBreadcrumb(),
-		    ));
-    }
-    
-    
-    private function getElementsByarrayIndex($array, $whatToFind) 
-    {
-	$return = array();
-	foreach ($array as $value)
-	{
-	    if (!empty($value[$whatToFind]))
-	    {
-		$return[] = $value[$whatToFind];
-	    }
-	}
-	return $return;
-	
-    }
-    
-    function getBreadcrumb() {
-        $this->_breadcrumbs['ad']['uri'] = $this->generateUrl('ad_manager_ad_homepage');
-        $path_info = $this->getRequest()->getPathInfo();
-        $breadcrumbs = array();
-        foreach ($this->_breadcrumbs as $key => $breadcrumb)
-        {
-            if (preg_match("#".$key."#", $path_info)) {
-                $breadcrumbs[] = $breadcrumb;
-            }
-        }
-        return $breadcrumbs;
-    }
+//
+//		$em = $this->getDoctrine()->getEntityManager();
+//		$em->persist($ad);
+//		$em->flush();
+//		
+//		return $this->redirect($this->generateUrl('ad_manager_ad_homepage'));
+//	    }
+//	}
+//	
+//	
+//	return $this->render('AdManagerPublisherBundle:Ad:add.html.twig', array(
+//            'form' => $form->createView(),
+//            'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
+//            'admin_pool'    => $this->container->get('sonata.admin.pool'),
+//            'breadcrumbs' => $this->getBreadcrumb(),
+//        ));
+//    }
+//    
+//    public function deleteAction($id, $confirm = 0)
+//    {
+//	$ad = $this->getDoctrine()
+//	    ->getRepository('AdManagerPublisherBundle:Ad')
+//	    ->find($id);
+//	if ($ad->getDeleted() == 1)
+//	{
+//	    return $this->redirect($this->generateUrl('ad_manager_ad_homepage'));
+//	}
+//	    
+//	$request = $this->getRequest();
+//	
+//	if ($request->getMethod() == 'POST') {
+//	    $form = $request->request->get('form');
+//	    
+//	    if ($form['confirm'] == 1)
+//	    {
+//		$em = $this->getDoctrine()->getEntityManager();
+//		$ad->setDeleted('1');
+//		$em->flush();
+//		return $this->redirect($this->generateUrl('ad_manager_ad_homepage'));
+//	    }
+//	    
+//	} else {
+//	    $form = $this->createFormBuilder()
+//		->add('confirm', 'checkbox', array('label' => 'Do you realy want to delete advertisment'))
+//		->getForm();
+//	}
+//	
+//	if (!$ad) {
+//	    throw $this->createNotFoundException('No ad found for id = '. $id);
+//	}
+//        return $this->render(
+//		'AdManagerPublisherBundle:Ad:delete.html.twig', 
+//		array(
+//		    'ad' => $ad,
+//		    'form' =>  $form->createView(),
+//                    'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
+//                    'admin_pool'    => $this->container->get('sonata.admin.pool'),
+//                    'breadcrumbs' => $this->getBreadcrumb(),
+//		    ));
+//    }
+//    
+//    public function editAction($id)
+//    {
+//	$ad = $this->getDoctrine()
+//	    ->getRepository('AdManagerPublisherBundle:Ad')
+//	    ->find($id);
+//	
+//	if (!$ad) {
+//	    throw $this->createNotFoundException('No advertisment found for id = '. $id);
+//	}
+//
+//	{
+//	    /**
+//	     * Составим массив полей объявления до внесения изменений
+//	     */
+//
+//	    $beforUpdateFields = array();
+//	    foreach ($ad->getFieldValues() as $fieldValue)
+//	    {
+//		$beforUpdateFields[$fieldValue->getId()] = $fieldValue;
+//	    }
+//	}
+//	
+//	$editForm = $this->createForm(new AdType(), $ad);
+//	$editForm->add('deleted', 'checkbox', array(
+//	    'label' => 'Is Deleted', 
+//	    'required' => false,
+//	    ));	
+//	$request = $this->getRequest();
+//	$em = $this->getDoctrine()->getEntityManager();
+//	
+//	if ($request->getMethod() == 'POST') {
+//	    $editForm->bindRequest($request);
+//
+//	    if ($editForm->isValid()) {
+//		$ad = $editForm->getData();
+//		
+//		if ( isset($_REQUEST['ad']['field_values']))
+//		{
+//		    $notDeletedIds = array_unique($this->getElementsByarrayIndex($_REQUEST['ad']['field_values'], 'id'));
+//
+//		    foreach($beforUpdateFields as $key => $value)
+//		    {
+//			if (!in_array($key, $notDeletedIds))
+//			{
+//			    $em->remove($value);
+//			}
+//		    }
+//		}
+////		$ad->setDeleted(0);
+//		$em->persist($ad);
+//		$em->flush();
+//		
+//		return $this->redirect($this->generateUrl('ad_manager_ad_homepage'));
+//	    }
+//	}
+//	
+//        return $this->render(
+//		'AdManagerPublisherBundle:Ad:edit.html.twig', 
+//		array(
+//		    'ad' => $ad,
+//		    'form' =>  $editForm->createView(),
+//                    'base_template' => $this->container->get('sonata.admin.pool')->getTemplate('layout'),
+//                    'admin_pool'    => $this->container->get('sonata.admin.pool'),
+//                    'breadcrumbs' => $this->getBreadcrumb(),
+//		    ));
+//    }
+//    
+//    
+//    private function getElementsByarrayIndex($array, $whatToFind) 
+//    {
+//	$return = array();
+//	foreach ($array as $value)
+//	{
+//	    if (!empty($value[$whatToFind]))
+//	    {
+//		$return[] = $value[$whatToFind];
+//	    }
+//	}
+//	return $return;
+//	
+//    }
+//    
+
     
 }
